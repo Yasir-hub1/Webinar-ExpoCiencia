b1205b4a12ddd9e05556488f42d0f24e93f0a6e9
Yasir <yasirarce70@gmail.com>
2022-11-15 23:55:36 -0400

    Añadiendo login y registro en la vista pricipal para el publico,creando tabla participante

---
+++ database/migrations/2022_11_16_032613_create_participantes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participantes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seminarios_id')
+            ->nullable()
+            ->constrained('seminarios')
+            ->cascadeOnUpdate()
+            ->nullOnDelete();
+
+            $table->string('nombre');
+            $table->string('foto');
+            $table->string('profesion');
+            $table->string('biografia');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participantes');
+    }
+}
